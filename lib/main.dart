7ea9be873db3a44e539d38d52f636da9a2213dca
CelioFS7 <celioferreiramlm@gmail.com>
2021-07-26 09:28:12 -0300

    Jogo Pedra papel Tesoura

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'package:pedra_papel_tesoura/Jogo.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: Jogo(),
+    debugShowCheckedModeBanner: false,
+
+  ));
+}
+
